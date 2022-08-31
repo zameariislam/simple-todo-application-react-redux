b46fc9dc00e6b51a1fe634512a09ed56d7782f6e
zameariislam <zameariislam@gmail.com>
2022-08-31 15:01:21 +0200

    input section added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import tickImage from "../images/double-tick.png";
 import noteImage from "../images/notes.png";
 import plusImage from "../images/plus.png";
+import { useDispatch } from "react-redux";
+import { added } from "../redux/todos/actions";
 
 export default function Header() {
+    
+    const [input,setInput]=useState('')
+    const dispatch=useDispatch()
+
+
+    const handleInput=(e)=>{
+        setInput(e.target.value)
+        
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        dispatch(added(input))
+        setInput('')
+
+    }
+
+
     return (
         <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+            <form onSubmit={handleSubmit} className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
                 <img src={noteImage} className="w-6 h-6" alt="Add todo" />
                 <input
                     type="text"
                     placeholder="Type your todo"
+                    value={input}
+                    onChange={ handleInput}
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
                 />
                 <button
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,47 @@ import Todo from "./Todo";
 import { useSelector } from "react-redux/es/exports";
 
 export default function TodoList() {
-    
-    const todos=useSelector((state)=>state.todos)
 
-    
+    const todos = useSelector((state) => state.todos)
+    const filters = useSelector((state) => state.filters)
+    const { status, colors } = filters
+
+    const filterByStatus=(todo )=> {
+        switch (status) {
+            case 'Complete':
+                return todo.completed
+            case 'InComplete':
+                return !todo.completed
+
+
+            default:
+                return todo;
+        }
+
+    }
+
+    const filterByColor=(todo) => {
+        if (colors.length > 0) {
+            return colors.includes(todo?.color)
+            
+
+        }
+        return true;
+
+
+
+    }
+
 
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {
-                todos.map(todo=><Todo todo={todo} key={todo.id} />)
+                todos
+                    .filter(filterByStatus)
+                    .filter(filterByColor)
+                    .map(todo => <Todo todo={todo} key={todo.id} />)
             }
-           
+
         </div>
     );
 }
