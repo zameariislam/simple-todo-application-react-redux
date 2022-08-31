7b0dc92fe007b4499c19fdf6c3fd261606102f07
zameariislam <zameariislam@gmail.com>
2022-08-31 13:10:02 +0200

    todo section done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,50 @@
+
+import { useSelector,useDispatch } from "react-redux";
+import { statusChanged } from "../redux/filters/actions";
+
+
 export default function Footer() {
+    const todos=useSelector((state)=>state.todos)
+    const filters=useSelector((state)=>state.filters)
+    const{status,colors}=filters
+    const dispatch=useDispatch()
+
+
+    const todosRemaining=()=>{
+      const taskLeft=  (todos.filter(todo=>!todo.completed))
+      switch (taskLeft.length) {
+        case 0:
+            
+            return 'No Task left'
+            case 1:
+            
+                return 'One Task left'
+      
+        default:
+            return `${taskLeft.length} Tasks left`
+      }
+     
+
+    }
+
+    const handleStatus=(status)=>{
+        dispatch(statusChanged(status))
+
+
+    }
+
+      
     return (
         <div className="mt-4 flex justify-between text-xs text-gray-500">
-            <p>2 tasks left</p>
+            <p>{todosRemaining()} </p>
             <ul className="flex space-x-1 items-center text-xs">
-                <li className="cursor-pointer font-bold">All</li>
+                <li onClick={()=>handleStatus('All')} className={`cursor-pointer ${status==='All' && 'font-bold'} `}>All</li>
                 <li>|</li>
-                <li className="cursor-pointer">Incomplete</li>
+                <li onClick={()=>handleStatus('InComplete')} className={`cursor-pointer ${status==='InComplete' 
+                && 'font-bold' }`}>Incomplete</li>
                 <li>|</li>
-                <li className="cursor-pointer">Complete</li>
+                <li onClick={()=>handleStatus('Complete')} className={`cursor-pointer ${status==='Complete' 
+                && 'font-bold' }`}>Complete</li>
                 <li></li>
                 <li></li>
                 <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,38 @@
 import cancelImage from "../images/cancel.png";
+import { useDispatch } from "react-redux";
+import { colorSeleted, deleted, toggled } from "../redux/todos/actions";
 
 export default function Todo({todo}) {
+
+    const dispatch=useDispatch()
     const{id,text,color,completed}=todo
+
+    const handleToggle=(todoId)=>{
+        dispatch(toggled(todoId))
+
+
+    }
+
+    const handleColorSelected=(todoId,color)=>{
+        dispatch(colorSeleted(todoId,color))
+       
+
+    }
+
+    const handleDelete=(todoId)=>{
+        dispatch(deleted(todoId))
+       
+
+    }
     return (
         <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
-            <div className="rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 border-green-500 focus-within:border-green-500">
+            <div className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0
+             justify-center items-center mr-2   ${completed && 'border-green-500 focus-within:border-green-500'} `}>
                 <input
                     type="checkbox"
                     className="opacity-0 absolute rounded-full"
-                    
+                    onChange={()=>handleToggle(id)}
+
                 />
                  {
                     completed && <svg
@@ -24,13 +48,19 @@ export default function Todo({todo}) {
                {text}
             </div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-green-500 hover:bg-green-500 bg-green-500"></div>
+            <div onClick={()=>handleColorSelected(id,'green')} className={`flex-shrink-0 h-4 w-4 rounded-full
+             border-2 ml-auto cursor-pointer border-green-500 
+             hover:bg-green-500 ${color==='green' && 'bg-green-500'} `}></div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500"></div>
+            <div onClick={()=>handleColorSelected(id,'yellow')} className={`flex-shrink-0 h-4 w-4 rounded-full 
+            border-2 ml-auto cursor-pointer border-yellow-500 hover:bg-yellow-500
+             ${color==='yellow' && 'bg-yellow-500'}`}></div>
 
-            <div className="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
+            <div  onClick={()=>handleColorSelected(id,'red')} className={`flex-shrink-0 h-4 w-4 rounded-full 
+            border-2 ml-auto cursor-pointer border-red-500 
+            hover:bg-red-500 ${color==='red'&& 'bg-red-500' }`}></div>
 
-            <img
+            <img onClick={()=>handleDelete(id)}
                 src={cancelImage}
                 className="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
                 alt="Cancel"
